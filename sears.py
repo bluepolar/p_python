6e72c89d104918c2bd6df190f88e312c058f8049
bluepolar <bluepolar05@gmail.com>
2025-03-02 20:50:54 +0900

    재구성

--- sears.py
+++ sears.py
@@ -1,11 +1,11 @@
-bill_thickness = 0.11 * 0.001 # 미터환산
-sears_height = 442 #시어스 타원 높이
-num_bills = 1
-day = 1
+bill_thickness = 0.11 * 0.001   # 미터환산
+sears_height   = 442              #시어스 타원 높이
+num_bills      = 1
+day            = 1
 
 while num_bills * bill_thickness < sears_height:
     print(day, num_bills, num_bills * bill_thickness)
-    day += 1
+    day = day + 1
     num_bills = num_bills * 2
 
 print('Number of days: ', day)
