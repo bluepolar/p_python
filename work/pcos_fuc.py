3eab6017ddeaac6ed56d0e964e76f1db92749a56
bluepolar <bluepolar05@gmail.com>
2025-03-03 17:29:25 +0900

    CSV 사용

--- work/pcos_fuc.py
+++ work/pcos_fuc.py
@@ -1,15 +1,18 @@
+import csv
 def portfolio_cost(filename):
-    num = 0
-    prices = 0.0
     total_cost = 0.0
-    f = open(filename, 'rt')
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        total_cost = total_cost + int(row[1]) * float(row[2])
+    
+    #f = open(filename, 'rt')
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                #row = line.split(',')
+                total_cost = total_cost + int(row[1]) * float(row[2])
+            except ValueError:
+                print('Bad row:', row)
 
-    f.close()
+    #f.close()
     return total_cost
 
-#cost = portfolio_cost('data/portfolio.csv')
-#print('Total cost: ', total_cost)
