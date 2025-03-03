ba6fd91f3a5dc4fd58677d7427058e2b9e9e5b52
bluepolar <bluepolar05@gmail.com>
2025-03-03 10:45:34 +0900

    f를 사용한 출력 정리

--- mortgage.py
+++ mortgage.py
@@ -23,5 +23,6 @@ while principal > 0:
     print(month, round(total_paid, 2), round(principal, 2))
 
 
-print('Total paid: ', round(total_paid, 2))
-print('Total months:', month)
+total_paid = round(total_paid, 2)
+print(f'Total paid  : {total_paid}')
+print(f'Total months: {month}')
