bc84c153e042e26096e6ec1eaaded83e3fb324ea
bluepolar <bluepolar05@gmail.com>
2025-03-06 23:59:04 +0900

    추가기능

--- work/pcost.py
+++ work/pcost.py
@@ -1,15 +1,29 @@
-f = open('data/portfolio.csv', 'rt')
-headers = next(f)
-num = 0
-prices = 0.0
-total_p = 0.0
-for line in f:
-    row = line.split(',')
+import csv
+def portfolio_cost(filename):
+    total_cost = 0.0
+    
+    #f = open(filename, 'rt')
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for rowno, row in enumerate(rows, start = 1):
+            record = dict(zip(headers, row))
+            try:
+                #row = line.split(',')
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                #total_cost = total_cost + int(row[1]) * float(row[2])
+                total_cost += nshares * price
+            except ValueError:
+                print('Bad row:', row)
 
-    num = num + int(row[1])
-    prices = prices + int(row[1]) * float(row[2])
-    total_p = total_p + float(row[2])
-    print(row)
+    #f.close()
+    return total_cost
 
-print('Total cost: ' + str(prices))
-#print(total_p)
+import sys
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'data/portfolio.csv'
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
