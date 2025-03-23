5eb77a8c6dc70689e028d9db8d0950db5d03757a
bluepolar <bluepolar05@gmail.com>
2025-03-23 10:24:44 +0900

    ex3.2

--- work/report.py
+++ work/report.py
@@ -47,7 +47,46 @@ def make_report(portfolio, prices):
         summary =(stock['name'], stock['shares'], current_price, change)
         rows.append(summary)
     return rows
+
+def make_report_data(portfolio, prices):
+    '''
+    포트폴리오 리스트와 가격 딕셔너리로 부터 주어진 name, shares, price,change
+    의 리스트를 만든다
+    '''
+    rows = []
+    for stock in portfolio:
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        summary =(stock['name'], stock['shares'], current_price, change)
+        rows.append(summary)
+    return rows
+
+def print_report(reportdata):
+    '''
+    name, shres, price, change 튜플 리스트를 이용해 형식화된 데이터 출력
+    '''
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print('%10s, %10s, %10s, %10s' % headers)
+    print(('-' * 10 + ' ') * len(headers))
+    for row in reportdata:
+        print('%10s %10s %10.2f %10.2f' % row)
+
+
+def portfolio_report(portfoliofile, pricefile):
+    '''
+    포트폴리오와 가격을 이용하여 보고서 생성
+    '''
+    # 데이터 파일 읽기
+    portfolio = read_portfolio(portfoliofile)
+    price = read_prices(pricefile)
+
+    # 보고서 데이터 생성
+    report = make_report_data(portfolio, price)
+
+    # 양식 출력
+    print_report(report)
     
+portfolio_report('data/portfoliodate.csv', 'data/prices.csv') 
         
 #portfolio = read_portfolio('data/portfolio.csv')
 portfolio = read_portfolio('data/portfoliodate.csv')
@@ -55,7 +94,7 @@ prices = read_prices('data/prices.csv')
 
 report = make_report(portfolio, prices)
 
-
+ 
 #포트폴리오 총 합
 total_cost = 0.0
 for s in portfolio:
@@ -68,5 +107,5 @@ for s in portfolio:
     total_value += s['shares'] * prices[s['name']]
 
 print('Current value: ', total_value)
-
+ 
 
